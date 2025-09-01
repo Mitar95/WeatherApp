7b481dbdda255ced705f5e70ad77601c221260b0
Митар Дражић <mitar@epiphanyvoice.io>
2025-09-01 13:45:54 +0200

    add template screens

---
+++ screens/ForecastScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ForecastScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Forecast Screen</Text>
+    </View>
+  );
+}
